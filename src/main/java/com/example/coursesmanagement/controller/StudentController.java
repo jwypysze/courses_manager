9968229697c804b070a1a8b9248f03dccc7e57fa
jwypysze <joanna.wypyszewska@gmail.com>
2023-08-02 21:22:32 +0200

    Security - dependency, SecurityConfig class

--- src/main/java/com/example/coursesmanagement/controller/StudentController.java
+++ src/main/java/com/example/coursesmanagement/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -65,4 +66,5 @@ public class StudentController {
         model.addAttribute("newRegistration", new RegistrationDto());
         return "/student/sign-up-for-course";
     }
+
 }
--- src/main/java/com/example/coursesmanagement/service/UserService.java
+++ src/main/java/com/example/coursesmanagement/service/UserService.java
@@ -1,14 +1,18 @@
 package com.example.coursesmanagement.service;
 
 import com.example.coursesmanagement.exception.exceptions.EntityNotFoundException;
+import com.example.coursesmanagement.model.dto.RegistrationDto;
 import com.example.coursesmanagement.model.dto.UserDto;
+import com.example.coursesmanagement.model.entity.RegistrationEntity;
 import com.example.coursesmanagement.model.entity.UserEntity;
 import com.example.coursesmanagement.repository.RegistrationJpaRepository;
 import com.example.coursesmanagement.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -47,6 +51,25 @@ public class UserService {
         }
         userJpaRepository.deleteUsersFromTableUsersNotifications(userEntity.getId());
         userJpaRepository.delete(userEntity);
+    }
+
+    public List<RegistrationDto> findRegistrationsByUser(UserDto userDto) {
+        UserEntity userEntity = userJpaRepository.findById(userDto.getId())
+                .orElseThrow(() -> new EntityNotFoundException(UserEntity.class, userDto.getId()));
+        List<Long> registrationsIdByUser = registrationJpaRepository.findRegistrationsIdByUser(userEntity);
+        List<RegistrationEntity> registrationEntities = new ArrayList<>();
+        for(Long registrationId : registrationsIdByUser) {
+            RegistrationEntity registrationEntity = registrationJpaRepository.findById(registrationId)
+                    .orElseThrow(() -> new EntityNotFoundException(RegistrationDto.class, registrationId));
+            registrationEntities.add(registrationEntity);
+        }
+        return registrationEntities.stream()
+                .map(registrationEntity ->
+                        new RegistrationDto(registrationEntity.getId(),
+                                registrationEntity.getDate(), registrationEntity.getTime(),
+                                registrationEntity.getUserEntity().getId(),
+                                registrationEntity.getCourseEntity().getId()))
+                .toList();
     }
 
     public void updateUserById(UserDto userDto) {
